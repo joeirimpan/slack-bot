a74845bd3e119fc05cce2d6ea183887c167ad7c1
Joe Paul <joe.paul@fulfil.io>
2016-09-26 00:30:38 +0530

    Slack bot client

--- slack_bot/slack_bot.py
+++ slack_bot/slack_bot.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+import requests
+import json
+
+class Slack:
+
+    def __init__(self, url):
+        self.base_url = url
+        self.session = requests.Session()
+
+    def notify(self, **kwargs):
+        return self.session.post(
+            self.base_url,
+            data={
+                'payload': json.dumps(kwargs)
+            }
+        )
+
